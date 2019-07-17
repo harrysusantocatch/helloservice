d4b04f729454b395b064520aa7015eb33701559c
Bolobulet <bolobulet@gmail.com>
2019-07-18 05:29:08 +0700

    change database

--- HelloService/Entities/DB/StatusApp.cs
+++ HelloService/Entities/DB/StatusApp.cs
@@ -1,8 +1,9 @@
 ﻿using Catcher.DB.DTO;
+using static HelloService.Helper.Constant;
 
 namespace HelloService.Entities.DB
 {
-    [DtoClass("CATCH_DB", "STATUS_APPLICATION")]
+    [DtoClass(DATABASE_NAME, "STATUS_APPLICATION")]
     public class StatusApp : Dto
     { 
         public string Status { get; set; }
